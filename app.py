cedcbaeeabe7f04dc1d67098cde9e1a0390e6fcc
corinneah <103472876+corinneah@users.noreply.github.com>
2022-12-05 00:26:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+df = pd.read_csv('data/NYPD_Arrest_Data__Year_to_Date_.csv')
+df = df.head(100)
+
+st.title('NYPD Arrest Data')
+st.text('Below shows a breakdown of every arrest effected in NYC by the NYPD in 2022')
+
+if st.checkbox('Show first 100 records of NYPD Arrest Dataset'):
+    st.dataframe(df)
+
+code = '''def test():
+    print("Hello, this is a testing code-block")'''
+st.code(code, language='python')
+
+st.subheader('Description of PD code')
+
+df = pd.read_csv('data/NYPD_Arrest_Data__Year_to_Date_.csv')
+
+pd_code = df['PD_DESC'].value_counts()
+pd_code
+
+st.caption("This is a summary of the descrption of arrests")
+st.write(df)
+
+hist_values = np.histogram(
+    df['value'], bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+st.subheader('Arrests in 2022')
+
+arrest_day = df['ARREST_DATE'].value_counts()
+
+st.line_chart(arrest_day)
